c8c5f981bde4d21aa494f37da52886d1177151c4
codeAKstan <boystanismoses@gmail.com>
2024-11-12 08:07:08 +0100

    Added password chanage

--- socWeb/registration/urls.py
+++ socWeb/registration/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('home/', views.dashboard, name='dashboard')
+    path('home/', views.dashboard, name='dashboard'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
     
 ]
