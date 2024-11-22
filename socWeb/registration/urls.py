aa56a59de88bacbc97ada67b1c728b4d74803a48
codeAKstan <boystanismoses@gmail.com>
2024-11-22 14:45:05 +0100

    Completed users profile edit

--- socWeb/registration/urls.py
+++ socWeb/registration/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('edit/', views.edit, name="edit")
 ]
--- socWeb/registration/views.py
+++ socWeb/registration/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
 
@@ -11,6 +12,7 @@ def register(request):
             new_user = form.save(commit=False)
             new_user.set_password(form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request,
                           'registration/register_done.html',
                           {'new_user': new_user})
@@ -26,3 +28,21 @@ def register(request):
 def dashboard(request):
     return render(request, 'registration/dashboard.html',
                   {'section': 'dashboard'})
+
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST,
+                                       files=request.FILES)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    
+    return render(request, 
+                  'registration/edit.html',
+                {'user_form': user_form,
+                'profile_form': profile_form})
